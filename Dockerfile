7951c74d7bb9430bb913f6b5d984ec09eadfce0e
Belogolov Dan <belogolov.dan@mail.ru>
2025-01-22 18:02:02 +0700

    save

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,6 @@
-FROM alpine:latest
+FROM alpine:latest as builder
 
-# Установим необходимые зависимости и PHP
 RUN apk --no-cache add \
-    bash \
-    nginx \
-    supervisor \
-    postgresql-client \
     postgresql-dev \
     nodejs \
     npm \
@@ -29,50 +24,71 @@ RUN apk --no-cache add \
     curl 
 
 RUN if [ ! -e /usr/bin/php ]; then ln -s /usr/bin/php83 /usr/bin/php; fi
-# Set working directory
+
 WORKDIR /opt/laravel
 
-
-# Install composer
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
-# Copy Nginx configuration
+COPY  laravel/ /opt/laravel
+
+RUN  composer install --no-interaction --no-scripts
+
+RUN npm install && npm run build
+
+
+FROM alpine:latest
+
+RUN apk --no-cache add \
+    nginx \
+    supervisor \
+    postgresql-client \
+    php \
+    php-fpm \
+    php-pdo \
+    php-pdo_pgsql \
+    php-opcache \
+    php-phar \
+    php-mbstring \
+    php-json \
+    php-curl \
+    php-dom \
+    php-tokenizer \
+    php-fileinfo \
+    php-openssl \
+    php-xml \
+    php-session \
+    php-simplexml \
+    php-xmlwriter \
+    curl 
+
+RUN if [ ! -e /usr/bin/php ]; then ln -s /usr/bin/php83 /usr/bin/php; fi
+
+WORKDIR /opt/laravel
+
 COPY conf.d/nginx/default.conf /etc/nginx/nginx.conf
-
-# Copy PHP configuration
 COPY conf.d/php-fpm/php-fpm.conf /usr/local/etc/php-fpm.d/www.conf
-
 COPY conf.d/php/php.ini /usr/local/etc/php/conf.d/php.ini
-
 COPY conf.d/php/opcache.ini /usr/local/etc/php/conf.d/opcache.ini
 
-# Copy Supervisor configuration
 COPY conf.d/supervisor/supervisord.conf /etc/supervisord.conf
 
-# Copy Laravel application files
-COPY  laravel/ /opt/laravel
+COPY --from=builder /opt/laravel /opt/laravel
 
 RUN set -x ; \
   addgroup -g 82 -S www-data ; \
   adduser -u 82 -D -S -G www-data www-data && exit 0 ; exit 1
-# Set up permissions
+  
 RUN chown -R www-data:www-data /opt/laravel \
     && chmod -R 775 /opt/laravel/storage && \
     chown -R www-data:www-data /opt/laravel/storage 
 
-
-# Scheduler setup
-
-# Create a log file
 RUN touch /var/log/cron.log
 
-# Add cron job directly to crontab
 RUN echo "* * * * * /usr/local/bin/php /opt/laravel/artisan schedule:run >> /var/log/cron.log 2>&1" | crontab -
 
-# Expose ports
 EXPOSE 80
 
-ADD entrypoint.sh /root/entrypoint.sh
+COPY entrypoint.sh /root/entrypoint.sh
 
 RUN chmod +x /root/entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,96 +1,36 @@
 #!/bin/sh
 
-# Set the base directory for the app
 BASEDIR=/opt/laravel
 
-# Flag to check if it's a fresh install
-FIRST_INSTALL=false
-
-# Check if the .env file exists
-if [ ! -f "$BASEDIR/.env" ]; then
-   FIRST_INSTALL=true
-fi
-
-# Use FIRST_INSTALL variable as needed in the rest of your script
-if [ "$FIRST_INSTALL" = true ]; then
-    echo "This is a FRESH INSTALL."
-
-    # Determine the appropriate environment file based on the PRODUCTION variable
-    if [ "$PRODUCTION" = "1" ]; then
-        ENV_FILE=".env.production"
-    else
-        ENV_FILE=".env.example"
-    fi
-
-    # Copy the chosen environment file to create the .env file
-    echo "Generating .env from a copy $ENV_FILE ..."
-    cp $ENV_FILE .env
-    echo "File .env generated."
-fi
-
-# Clear the old boostrap/cache
-php artisan clear-compiled
-
-# Install composer dependencies
-if [ "$PRODUCTION" = "1" ]; then
-    composer install --no-dev --no-interaction --no-scripts
-else
-    composer install --no-interaction --no-scripts
-fi
-
-echo "Installing npm dependencies..."
-npm install
-
-# Build frontend assets
-echo "Building frontend assets..."
-npm run build
-
-
-
-echo "Generating app key..."
-# Generate an application key
-php artisan key:generate --force
-echo "App key generated."
-
-# Clear the optimization cache
-php artisan optimize:clear
-
-# Remove prior storage links that exist
-rm -rf public/storage
-
-# Build up a new storage link
-php artisan storage:link
-
-# Set permissions
-echo "Setting file permissions..."
-chown -R www-data:www-data /opt/laravel
-chmod -R 755 /opt/laravel/storage
-chmod -R 777 /opt/laravel/storage/framework/views/
-
-
-# Wait for the database to be ready (PostgreSQL in this case)
 echo "Waiting for the database to be ready..."
 until pg_isready -h db -p 5432 -U user; do
     echo 'Waiting for database...';
     sleep 2;
 done
 
-# Run database migrations
-echo "Running migrations..."
 php artisan migrate --force
 
-# Run tests
-echo "Running tests..."
+
+php artisan clear-compiled
+
+
+php artisan key:generate --force
+
+
+php artisan optimize:clear
+
+rm -rf public/storage
+
+php artisan storage:link
+
+chown -R www-data:www-data /opt/laravel
+chmod -R 755 /opt/laravel/storage
+chmod -R 777 /opt/laravel/storage/framework/views/
+
+
+php artisan migrate --force
+
 php artisan test
 
 
-
-# Check if running in production or development mode
-if [ "$PRODUCTION" = "1" ]; then
-    echo "Running in production mode"
-else
-    echo "Running in development mode"
-fi
-
-# Start supervisord
 exec /usr/bin/supervisord -c /etc/supervisord.conf
